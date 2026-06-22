8bc23c9a3b4a18084c6e0ceff461ab7a03dff90d
atorrao <andre@addapters.com>
2026-06-22 17:11:51 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from '@/components/layout/Layout'
-import Dashboard    from '@/components/modules/Dashboard'
-import Clients      from '@/components/modules/Clients'
-import Portfolios   from '@/components/modules/Portfolios'
-import Properties   from '@/components/modules/Properties'
+import Dashboard      from '@/components/modules/Dashboard'
+import Clients        from '@/components/modules/Clients'
+import Portfolios     from '@/components/modules/Portfolios'
+import Properties     from '@/components/modules/Properties'
 import PropertyDetail from '@/components/modules/PropertyDetail'
-import Billing      from '@/components/modules/Billing'
-import MarketSearch from '@/components/modules/MarketSearch'
+import PropertyMap    from '@/components/modules/PropertyMap'
+import Billing        from '@/components/modules/Billing'
+import MarketSearch   from '@/components/modules/MarketSearch'
 import DataTapeImport from '@/components/modules/DataTapeImport'
 
 export default function App() {
@@ -14,14 +15,15 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="/dashboard" replace />} />
-        <Route path="dashboard"           element={<Dashboard />} />
-        <Route path="clients"             element={<Clients />} />
-        <Route path="portfolios"          element={<Portfolios />} />
-        <Route path="properties"          element={<Properties />} />
-        <Route path="properties/:id"      element={<PropertyDetail />} />
-        <Route path="billing"             element={<Billing />} />
-        <Route path="market"              element={<MarketSearch />} />
-        <Route path="import"              element={<DataTapeImport />} />
+        <Route path="dashboard"      element={<Dashboard />} />
+        <Route path="clients"        element={<Clients />} />
+        <Route path="portfolios"     element={<Portfolios />} />
+        <Route path="properties"     element={<Properties />} />
+        <Route path="properties/:id" element={<PropertyDetail />} />
+        <Route path="map"            element={<PropertyMap />} />
+        <Route path="billing"        element={<Billing />} />
+        <Route path="market"         element={<MarketSearch />} />
+        <Route path="import"         element={<DataTapeImport />} />
       </Route>
     </Routes>
   )
